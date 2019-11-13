01bde46235f5baab681e9a8d39d56ec0edecdff8
limingru <18813070947@163.com>
2019-11-13 17:29:47 +0800

    简书项目第一次预览

---
+++ precache-manifest.b115acfe3647faa4ae081c9c9d564f1f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "278e37ea838409ee040fdfc42cf968d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb36390143f4b2af9d2",
+    "url": "/static/js/2.ce57a3e7.chunk.js"
+  },
+  {
+    "revision": "33b59bafe8485a1a7b64",
+    "url": "/static/js/main.f48f1356.chunk.js"
+  },
+  {
+    "revision": "3f6bf538a5b940142fb4",
+    "url": "/static/js/runtime-main.8f732c6c.js"
+  }
+]);
