86b07668f928f29a6771f9a3cf8a6610b425b0cb
limingru <18813070947@163.com>
2019-11-13 17:58:09 +0800

    更改打包公共路径后重新上传 github

---
+++ precache-manifest.1cff8eb1c05d17d45a5235c32a258bbe.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44ee7d0f158f0ec1dda6a6a567a41dd6",
+    "url": "/jianshu-preview/index.html"
+  },
+  {
+    "revision": "32cb3a8fa19c98db9884",
+    "url": "/jianshu-preview/static/js/2.a4bb17cd.chunk.js"
+  },
+  {
+    "revision": "33b59bafe8485a1a7b64",
+    "url": "/jianshu-preview/static/js/main.f48f1356.chunk.js"
+  },
+  {
+    "revision": "ea6fc78831fffa8bc88b",
+    "url": "/jianshu-preview/static/js/runtime-main.3c9c30e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b115acfe3647faa4ae081c9c9d564f1f.js"
+  "/jianshu-preview/precache-manifest.1cff8eb1c05d17d45a5235c32a258bbe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jianshu-preview/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
